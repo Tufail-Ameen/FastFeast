9035568119ef902087ce769b3fe377102a5bd921
APPLE <apple@192.168.1.17>
2025-06-11 16:55:00 +0500

    routing handel

--- src/app/component/logincomponent/logincomponent.component.ts
+++ src/app/component/logincomponent/logincomponent.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-logincomponent',
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, RouterLink],
   templateUrl: './logincomponent.component.html',
   styleUrl: './logincomponent.component.css'
 })
 export class LogincomponentComponent {
 
 }
- 
