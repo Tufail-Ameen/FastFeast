b80e4485a182edb49cfda26ec1f024c276716914
APPLE <apple@192.168.1.16>
2025-06-19 19:11:10 +0500

    Form Data Handeling

--- src/app/component/register-com/register-com.component.ts
+++ src/app/component/register-com/register-com.component.ts
@@ -1,18 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicModule} from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 // import { IonRadio, IonRadioGroup } from '@ionic/angular/standalone';
 
 
 @Component({
   selector: 'app-register-com',
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule],
   templateUrl: './register-com.component.html',
   styleUrls: ['./register-com.component.scss'],
 })
-export class RegisterComComponent  implements OnInit {
+export class RegisterComComponent implements OnInit {
+
+  registerData = {
+    firstName: '',
+    lastName: '',
+    gender: 'male',
+    language: {
+      english: true,
+      spanish: false
+    },
+    email: '',
+    phoneNo: '',
+    city: 'lahore',
+    password: '',
+    description: '',
+  }
+
+  onSubmit(data: any) {
+    console.log("Form data:", data);
+  }
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
