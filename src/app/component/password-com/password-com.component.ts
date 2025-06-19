036b14cff0be8628098d1351416de0ed06c6086e
APPLE <apple@192.168.1.16>
2025-06-19 15:11:09 +0500

    form data handeling in password screen

--- src/app/component/password-com/password-com.component.ts
+++ src/app/component/password-com/password-com.component.ts
@@ -1,16 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { CLIENT_RENEG_LIMIT } from 'tls';
 
 @Component({
   selector: 'app-password-com',
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule, FormsModule],
   templateUrl: './password-com.component.html',
   styleUrls: ['./password-com.component.scss'],
 })
-export class PasswordComComponent  implements OnInit {
+export class PasswordComComponent implements OnInit {
+
+  changePasswordData = {
+    newPassword: '',
+    confirmPassword: ''
+  }
+
+  onSubmit(data: any) {
+    console.log("Form data:", data);
+  }
+
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
